813b65b79c71909e6290c8c9a79acddf1e58ecdb
Jan <jan.farkas.sk@gmail.com>
2026-03-31 03:55:25 +0200

    creating input
    removing new input system( because on mouse down dont work othewise, and calculating the angle of the swipe

--- Matching/Assets/Scripts/Gem.cs
+++ Matching/Assets/Scripts/Gem.cs
@@ -5,6 +5,11 @@ public class Gem : MonoBehaviour
     public TileSpawner board;
     public Vector2Int gemPos;
     public TileSpawner.GemType gemType;
+
+    private Vector2 firstTouchPos;
+    private Vector2 finalTouchPos;
+    private bool mousePressed;
+    private float swipeAngle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,12 +19,31 @@ public class Gem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mousePressed&& Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+            finalTouchPos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+
+        }
     }
 
     public void SetUpGem(Vector2Int pos, TileSpawner _board)
     {
         gemPos= pos;
         board= _board;
+    }
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse Down");
+        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+    public void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(firstTouchPos.x-finalTouchPos.x,firstTouchPos.y-finalTouchPos.y);
+        swipeAngle = swipeAngle*180/Mathf.PI;
+        Debug.Log(swipeAngle);
     }
 }
